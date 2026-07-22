fc217158dc0e5c521eb9845c3ffab5213d883637
Misty <hiromu1018ks@gmail.com>
2026-07-22 23:51:31 +0000

    fix

--- modules/common.nix
+++ modules/common.nix
@@ -6,6 +6,34 @@
     "flakes"
   ];
 
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc.lib
+    zlib
+    glib
+    nss
+    nspr
+    atk
+    cups
+    dbus
+    expat
+    alsa-lib
+    mesa
+    gtk3
+    pango
+    cairo
+    xorg.libX11
+    xorg.libxcb
+    xorg.libXcomposite
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXrandr
+    xorg.libXrender
+    xorg.libXScrnSaver
+    xorg.libXtst
+  ];
+
   environment.systemPackages =
     with pkgs; [
       vim
