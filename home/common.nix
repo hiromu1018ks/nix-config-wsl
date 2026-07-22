9889724f8d6bbc7f8cda638f99e70c0ce6f09545
Misty <hiromu1018ks@gmail.com>
2026-07-22 05:38:38 +0000

    git setting

--- home/common.nix
+++ home/common.nix
@@ -1,5 +1,20 @@
-{ ... }:
+{ config, ... }:
 
 {
   programs.home-manager.enable = true;
+
+  programs.ssh = {
+    enable = true;
+    addKeysToAgent = "yes";
+    controlMaster = "auto";
+    controlPersist = "10m";
+    matchBlocks.github = {
+      hostname = "github.com";
+      user = "git";
+      identityFile = "${config.home.homeDirectory}/.ssh/id_ed25519";
+      identitiesOnly = true;
+    };
+  };
+
+  services.ssh-agent.enable = true;
 }
